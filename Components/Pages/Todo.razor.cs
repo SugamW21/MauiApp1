bb4f213e6b31ec91e840f3c2b434d80d24478e40
SugamW21 <wostisugam@gmail.com>
2024-12-15 12:04:53 +0545

    first commit

---
+++ Components/Pages/Todo.razor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using MauiApp1.Models;
+
+namespace MauiApp1.Components.Pages
+{
+    public partial class Todo : ComponentBase
+    {
+        private string newTask = string.Empty;
+        private List<TodoItem> todoList = new();
+        private string message = string.Empty;
+
+        private void AddTask()
+        {
+            if (!string.IsNullOrWhiteSpace(newTask))
+            {
+                todoList.Add(new TodoItem { Id = Guid.NewGuid(), TaskName = newTask, IsCompleted = false });
+                newTask = string.Empty;
+                message = "Task added successfully!";
+            }
+        }
+
+
+
+
+    }
+}
